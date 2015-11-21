8dd564cda201c80f7968f8f572af8b826c6ae212
trsh <janis.taranda@gmail.com>
2015-11-21 20:43:33 +0200

    Initial commit

---
+++ assets/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider').slider({indicators: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
